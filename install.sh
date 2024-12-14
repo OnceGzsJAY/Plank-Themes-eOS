973d658d8d176d5bd1e23a84c987ef708e3a2fc4
OnceGzsJAY <guozisheng2022@126.com>
2024-12-14 11:58:46 +0800

    Fix install.sh
    Can running in Ubuntu24.04

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ read -p "Do you want to install BigSur [Y/n]?" yn
 if [[ $yn =~ ^[Yy]$ ]] || [[ $yn == "" ]]; then
 
     echo "Copying plank theme..."
-    cp -Ri BigSur $HOME/.local/share/plank/themes
+    cp -Ri BigSur $HOME/.local/share/themes
 
     echo
     echo "Done"
